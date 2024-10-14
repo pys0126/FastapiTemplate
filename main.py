0887feff9757a860adad62c39ed822551a834798
uodrad <2493919891@qq.com>
2024-10-14 10:31:04 +0800

    优化启动方式

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-import os
-import sys
+import uvicorn
+from application import app
+from argparse import ArgumentParser, Namespace
 from application.config.ServerConfig import ServerConfig
 
-# 定义启动命令
-command: str = (f"uvicorn application:app --workers {ServerConfig.workers} --host {ServerConfig.host} "
-                f"--port {ServerConfig.port}")
+# 创建 ArgumentParser 对象
+parser: ArgumentParser = ArgumentParser(description="启动ASGI服务器")
+# 添加参数
+parser.add_argument("start_mode", type=str, choices=["pro", "dev", "debug"],
+                    help="启动模式: [pro：生产模式，dev：开发模式（自动重载），debug：调试模式（IDE可调试）]")
+# 解析命令行参数
+args: Namespace = parser.parse_args()
 
 if __name__ == "__main__":
-    if sys.argv[1] == "dev":
-        command = command + " --reload"
-    # 启动服务
-    os.system(command=command)
+    params: dict = {
+        "app": app,
+        "host": ServerConfig.host,
+        "port": ServerConfig.port
+    }
+    if args.start_mode == "dev":
+        params.update(app="application:app")
+        params.update(reload=True)
+    elif args.start_mode == "pro":
+        params.update(app="application:app")
+        params.update(workers=ServerConfig.workers)
+    # 启动ASGI服务器
+    uvicorn.run(**params)
+
 
